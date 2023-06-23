9871642a6518057497eb407dc9475641e164a072
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-06-23 18:09:55 +0200

    Update Sprite.js
    ChatGPT: replace x and y with a 2d vector class

--- JS game engine/Sprite.js
+++ JS game engine/Sprite.js
@@ -1,19 +1,16 @@
 class Sprite {
-  constructor(imagePath, x, y, width, height) {
+  constructor(imagePath, position, size) {
     this.image = new Image();
     this.image.src = imagePath;
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
+    this.position = position;
+    this.size = size;
   }
 
   draw(context) {
-    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    context.drawImage(this.image, this.position.x, this.position.y, this.size.x, this.size.y);
   }
 
-  move(x, y) {
-    this.x += x;
-    this.y += y;
+  move(vector) {
+    this.position = this.position.add(vector);
   }
 }
