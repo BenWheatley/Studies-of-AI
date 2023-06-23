3b8a39eb7450fac110effa75f459ec4986ade21b
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-06-23 22:06:44 +0200

    Update Sprite.js
    Ben: as ChatGPT was constantly trying to make new canvas elements on the DOM, I had to do a lot manually to get it re-focus it on the task I actually wanted to solve

--- JS game engine/Sprite.js
+++ JS game engine/Sprite.js
@@ -1,37 +1,52 @@
 class Sprite {
-  constructor(imagePath, position, size) {
-    this.image = new Image();
-    this.image.src = imagePath;
-    this.position = position;
-    this.size = size;
-  }
+	constructor(imageBitmap, position, size, context) {
+		this.imageBitmap = imageBitmap;
+		this.position = position;
+		this.size = size;
+		this.context = context;
+		this.createPixelDataFromCurrentBitmap();
+	}
+	
+	createPixelDataFromCurrentBitmap() {
+		const offscreen = new OffscreenCanvas(this.imageBitmap.width, this.imageBitmap.height);
+		const context = offscreen.getContext('bitmaprenderer');
+		context.transferFromImageBitmap(this.imageBitmap);
+		const imageData = context.getImageData(
+			0, 0,
+			this.imageBitmap.width, this.imageBitmap.height
+		);
+		this.pixelData = imageData.data;
+	}
 
-  draw(context) {
-    context.drawImage(this.image, this.position.x, this.position.y, this.size.x, this.size.y);
-  }
-
-  moveBy(vector) {
-    this.position = this.position.add(vector);
-  }
-
-  hitTest(point) {
-    const spriteTopLeft = this.position;
-    const spriteBottomRight = this.position.add(this.size);
-    
-    // check if point is within sprite bounds
-    if (point.x < spriteTopLeft.x || point.x > spriteBottomRight.x ||
-        point.y < spriteTopLeft.y || point.y > spriteBottomRight.y) {
-      return false;
-    }
-    
-    // calculate the pixel position within the image
-    const pixelX = Math.floor((point.x - spriteTopLeft.x) * (this.image.width / this.size.x));
-    const pixelY = Math.floor((point.y - spriteTopLeft.y) * (this.image.height / this.size.y));
-    
-    // get the pixel data and check alpha value
-    const context = document.createElement('canvas').getContext('2d');
-    context.drawImage(this.image, 0, 0, this.image.width, this.image.height);
-    const pixelData = context.getImageData(pixelX, pixelY, 1, 1).data;
-    return pixelData[3] > 0;
-  }
+	draw() {
+		this.context.drawImage(
+			this.imageBitmap,
+			this.position.x, this.position.y,
+			this.size.x, this.size.y
+		);
+	}
+	
+	moveBy(vector) {
+		this.position = this.position.add(vector);
+	}
+	
+	hitTest(point) {
+		const spriteTopLeft = this.position;
+		const spriteBottomRight = this.position.add(this.size);
+	
+		// Check if point is within sprite bounds
+		if (point.x < spriteTopLeft.x || point.x > spriteBottomRight.x ||
+			point.y < spriteTopLeft.y || point.y > spriteBottomRight.y) {
+			return false;
+		}
+		
+		// Calculate the pixel position within the image
+		const pixelX = Math.floor((point.x - spriteTopLeft.x) * (this.imageBitmap.width / this.size.x));
+		const pixelY = Math.floor((point.y - spriteTopLeft.y) * (this.imageBitmap.height / this.size.y));
+		
+		// Get the pixel data and check alpha value
+		const index = (pixelY * this.image.width + pixelX) * 4;
+		const alpha = this.pixelData[index + 3];
+		return alpha > 0;
+	}
 }
