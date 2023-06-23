9c5af6ea8027e0a7615d5bc752373d485ada8f3d
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-06-23 18:10:58 +0200

    Update Sprite.js
    ChatGPT: create a hit-test function for this sprite class, true only when specified 2d vector is on a pixel whose alpha value is not transparent; account for the possibility that the width and height of the image itself is different from the width and height of the sprite

--- JS game engine/Sprite.js
+++ JS game engine/Sprite.js
@@ -13,4 +13,25 @@ class Sprite {
   moveBy(vector) {
     this.position = this.position.add(vector);
   }
+
+  hitTest(point) {
+    const spriteTopLeft = this.position;
+    const spriteBottomRight = this.position.add(this.size);
+    
+    // check if point is within sprite bounds
+    if (point.x < spriteTopLeft.x || point.x > spriteBottomRight.x ||
+        point.y < spriteTopLeft.y || point.y > spriteBottomRight.y) {
+      return false;
+    }
+    
+    // calculate the pixel position within the image
+    const pixelX = Math.floor((point.x - spriteTopLeft.x) * (this.image.width / this.size.x));
+    const pixelY = Math.floor((point.y - spriteTopLeft.y) * (this.image.height / this.size.y));
+    
+    // get the pixel data and check alpha value
+    const context = document.createElement('canvas').getContext('2d');
+    context.drawImage(this.image, 0, 0, this.image.width, this.image.height);
+    const pixelData = context.getImageData(pixelX, pixelY, 1, 1).data;
+    return pixelData[3] > 0;
+  }
 }
