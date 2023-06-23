7f765abab4edecc1c51c48ef367789bdf8a13c21
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-06-24 00:41:32 +0200

    rm all related to hitTest,

--- JS game engine/Sprite.js
+++ JS game engine/Sprite.js
@@ -4,7 +4,6 @@ class Sprite {
 		this.position = position;
 		this.size = size;
 		this.context = context;
-		this.createPixelDataFromCurrentBitmap();
 	}
 	
 	static async buildSprite(imageUrl, position, size, context) {
@@ -14,46 +13,11 @@ class Sprite {
 		return new Sprite(imageBitmap, position, size, context);
 	}
 	
-	createPixelDataFromCurrentBitmap() {
-		const offscreen = new OffscreenCanvas(this.imageBitmap.width, this.imageBitmap.height);
-		const context = offscreen.getContext('bitmaprenderer');
-		context.transferFromImageBitmap(this.imageBitmap);
-		const imageData = context.getImageData(
-			0, 0,
-			this.imageBitmap.width, this.imageBitmap.height
-		);
-		this.pixelData = imageData.data;
-	}
-	
 	draw() {
 		this.context.drawImage(
 			this.imageBitmap,
 			this.position.x, this.position.y,
 			this.size.x, this.size.y
 		);
-	}
-	
-	moveBy(vector) {
-		this.position = this.position.add(vector);
-	}
-	
-	hitTest(point) {
-		const spriteTopLeft = this.position;
-		const spriteBottomRight = this.position.add(this.size);
-	
-		// Check if point is within sprite bounds
-		if (point.x < spriteTopLeft.x || point.x > spriteBottomRight.x ||
-			point.y < spriteTopLeft.y || point.y > spriteBottomRight.y) {
-			return false;
-		}
-		
-		// Calculate the pixel position within the image
-		const pixelX = Math.floor((point.x - spriteTopLeft.x) * (this.imageBitmap.width / this.size.x));
-		const pixelY = Math.floor((point.y - spriteTopLeft.y) * (this.imageBitmap.height / this.size.y));
-		
-		// Get the pixel data and check alpha value
-		const index = (pixelY * this.imageBitmap.width + pixelX) * 4;
-		const alpha = this.pixelData[index + 3];
-		return alpha > 0;
 	}
 }
