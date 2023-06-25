cb8db8169868124e2796b1010a939608854f07bf
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-06-25 13:10:34 +0200

    Create GPTEngine.js
    GPT: (with multiple prompts) make a singleton class with enterFullScreen and exitFullScreen methods

---
+++ JS game engine/GPTEngine.js
@@ -0,0 +1,38 @@
+class GPTEngine {
+	constructor(document) {
+		if (!GPTEngine.instance) {
+			this._document = document;
+			GPTEngine.instance = this;
+		}
+		
+		return GPTEngine.instance;
+	}
+	
+	enterFullScreen(element) {
+		if (element.requestFullscreen) {
+			element.requestFullscreen();
+		} else if (element.mozRequestFullScreen) { // Firefox
+			element.mozRequestFullScreen();
+		} else if (element.webkitRequestFullscreen) { // Chrome, Safari, Opera
+			element.webkitRequestFullscreen();
+		} else if (element.msRequestFullscreen) { // IE/Edge
+			element.msRequestFullscreen();
+		}
+	}
+	
+	exitFullScreen() {
+		if (this._document.exitFullscreen) {
+			this._document.exitFullscreen();
+		} else if (this._document.mozCancelFullScreen) { // Firefox
+			this._document.mozCancelFullScreen();
+		} else if (this._document.webkitExitFullscreen) { // Chrome, Safari, Opera
+			this._document.webkitExitFullscreen();
+		} else if (this._document.msExitFullscreen) { // IE/Edge
+			this._document.msExitFullscreen();
+		}
+	}
+	
+	static getInstance(document) {
+		return GPTEngine.instance || new GPTEngine(document);
+	}
+}
