d280e46fb7106f63de388b353f24ab2bd2db8079
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-06-25 19:44:47 +0200

    Update Sprite.js
    ChatGPT: "change it so I can easily debug if some sprite is stuck or failed to load"

--- JS game engine/Sprite.js
+++ JS game engine/Sprite.js
@@ -7,29 +7,39 @@ class Sprite {
 	}
 	
 	static cache = {}; // Static cache object to store image bitmaps
-	static loadingSprites = 0; // Counter for tracking loading sprites
+	static loadingSprites = {}; // Object to store loading sprites status
 	
 	static async buildSprite(imageUrl, position, size, context) {
 		let imageBitmap = Sprite.cache[imageUrl]; // Check if the image is already cached
 		
 		if (!imageBitmap) {
-			Sprite.loadingSprites++; // Increment the loadingSprites counter
+			Sprite.loadingSprites[imageUrl] = true; // Set loading status to true
 			
-			const response = await fetch(imageUrl);
-			const blob = await response.blob();
-			imageBitmap = await createImageBitmap(blob);
-			
-			// Cache the image bitmap for future use
-			Sprite.cache[imageUrl] = imageBitmap;
-			
-			Sprite.loadingSprites--; // Decrement the loadingSprites counter
+			try {
+				const response = await fetch(imageUrl);
+				const blob = await response.blob();
+				imageBitmap = await createImageBitmap(blob);
+				
+				// Cache the image bitmap for future use
+				Sprite.cache[imageUrl] = imageBitmap;
+				
+				delete Sprite.loadingSprites[imageUrl]; // Remove loading status on success
+			} catch (error) {
+				console.error(`Failed to load sprite: ${imageUrl}`);
+				delete Sprite.loadingSprites[imageUrl]; // Remove loading status on failure
+				throw error; // Rethrow the error to propagate it
+			}
 		}
 		
 		return new Sprite(imageBitmap, position, size, context);
 	}
 	
 	static stillLoading() {
-		return Sprite.loadingSprites > 0;
+		const loadingSprites = Object.keys(Sprite.loadingSprites);
+		if (loadingSprites.length > 0) {
+			console.log(`Stuck loading sprites: ${loadingSprites.join(", ")}`);
+		}
+		return loadingSprites.length > 0;
 	}
 	
 	draw() {
