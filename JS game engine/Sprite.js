af205439d4aa1523b130a1907730e693dabf9f71
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-06-25 19:42:52 +0200

    Update Sprite.js
    ChatGPT: add a stillLoading() static function which returns true when and only when there is a sprite that has not yet finished being built

--- JS game engine/Sprite.js
+++ JS game engine/Sprite.js
@@ -7,20 +7,29 @@ class Sprite {
 	}
 	
 	static cache = {}; // Static cache object to store image bitmaps
+	static loadingSprites = 0; // Counter for tracking loading sprites
 	
 	static async buildSprite(imageUrl, position, size, context) {
 		let imageBitmap = Sprite.cache[imageUrl]; // Check if the image is already cached
 		
 		if (!imageBitmap) {
+			Sprite.loadingSprites++; // Increment the loadingSprites counter
+			
 			const response = await fetch(imageUrl);
 			const blob = await response.blob();
 			imageBitmap = await createImageBitmap(blob);
 			
 			// Cache the image bitmap for future use
 			Sprite.cache[imageUrl] = imageBitmap;
+			
+			Sprite.loadingSprites--; // Decrement the loadingSprites counter
 		}
 		
 		return new Sprite(imageBitmap, position, size, context);
+	}
+	
+	static stillLoading() {
+		return Sprite.loadingSprites > 0;
 	}
 	
 	draw() {
