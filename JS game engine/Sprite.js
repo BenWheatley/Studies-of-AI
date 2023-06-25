91d2877786d152165bfa81d699fb4ae64bee3b56
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-06-25 19:38:00 +0200

    Update Sprite.js
    ChatGPT: "modify buildSprite so that image is only fetched once, and then cached for subesequent calls"
    
    (and then followed by the contents of Sprite.js)

--- JS game engine/Sprite.js
+++ JS game engine/Sprite.js
@@ -6,18 +6,30 @@ class Sprite {
 		this.context = context;
 	}
 	
+	static cache = {}; // Static cache object to store image bitmaps
+	
 	static async buildSprite(imageUrl, position, size, context) {
-		const response = await fetch(imageUrl);
-		const blob = await response.blob();
-		const imageBitmap = await createImageBitmap(blob);
+		let imageBitmap = Sprite.cache[imageUrl]; // Check if the image is already cached
+		
+		if (!imageBitmap) {
+			const response = await fetch(imageUrl);
+			const blob = await response.blob();
+			imageBitmap = await createImageBitmap(blob);
+			
+			// Cache the image bitmap for future use
+			Sprite.cache[imageUrl] = imageBitmap;
+		}
+		
 		return new Sprite(imageBitmap, position, size, context);
 	}
 	
 	draw() {
 		this.context.drawImage(
 			this.imageBitmap,
-			this.position.x, this.position.y,
-			this.size.x, this.size.y
+			this.position.x,
+			this.position.y,
+			this.size.x,
+			this.size.y
 		);
 	}
 }
