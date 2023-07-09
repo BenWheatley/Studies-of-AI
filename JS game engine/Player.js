390bfcba0e6127fd0c56b1cb12505d4913d686cd
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-07-09 22:19:55 +0200

    Update Player.js
    GPT: Modify the movement functions to account for the update function they're being called from having a deltaTime argument
    
    (Note: the previous issue with `if (this.velocity.magnitude() < 0) {` is back because I didn't tell it that it had made a mistake last time; it really doesn't understand what it's doing wrong here so I'll fix it again separately)

--- JS game engine/Player.js
+++ JS game engine/Player.js
@@ -3,38 +3,41 @@ class Player extends GameEntity {
   static size = new Vector2D(48, 48);
   static acceleration = 0.1;
   static deceleration = 0.05;
-  static maxSpeed = 100/1000.0; // pixels per millisecond
+  static maxSpeed = 5;
   static rotationalSpeed = 0.1;
 
   constructor() {
     super(new Vector2D(0, 0), 0, new Vector2D(), Player.size, Player.imageUrl);
   }
 
-  accelerateForward() {
+  accelerateForward(deltaTime) {
     const accelerationVector = new Vector2D(
       Math.cos(this.sprite.rotation) * Player.acceleration,
       Math.sin(this.sprite.rotation) * Player.acceleration
     );
-    this.velocity = this.velocity.add(accelerationVector);
-    if (this.velocity.mag() > Player.maxSpeed) {
+    const velocityChange = accelerationVector.multiply(deltaTime);
+    this.velocity = this.velocity.add(velocityChange);
+    if (this.velocity.magnitude() > Player.maxSpeed) {
       this.velocity = this.velocity.normalize().multiply(Player.maxSpeed);
     }
   }
 
-  decelerate() {
+  decelerate(deltaTime) {
     const decelerationVector = this.velocity.normalize().multiply(-Player.deceleration);
-    if (decelerationVector.mag() > this.velocity.mag()) {
+    const velocityChange = decelerationVector.multiply(deltaTime);
+    this.velocity = this.velocity.add(velocityChange);
+    if (this.velocity.magnitude() < 0) {
       this.velocity = new Vector2D();
-    } else {
-      this.velocity = this.velocity.add(decelerationVector);
     }
   }
 
-  turnLeft() {
-    this.sprite.rotation -= Player.rotationalSpeed;
+  turnLeft(deltaTime) {
+    this.sprite.rotation -= Player.rotationalSpeed * deltaTime;
   }
 
-  turnRight() {
-    this.sprite.rotation += Player.rotationalSpeed;
+  turnRight(deltaTime) {
+    this.sprite.rotation += Player.rotationalSpeed * deltaTime;
   }
 }
+
+// Other Player class methods or properties
