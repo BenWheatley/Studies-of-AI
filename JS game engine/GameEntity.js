1180cf27e88c800fad9ae41e20799fb132a9d599
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-07-09 21:40:37 +0200

    Create GameEntity.js
    Both: Make new GameEntity class that will be supertype of player and alien later

---
+++ JS game engine/GameEntity.js
@@ -0,0 +1,17 @@
+class GameEntity {
+  constructor(position, rotation, velocity, size, imageUrl) {
+    this.sprite = new Sprite(imageUrl, position, size);
+    this.sprite.rotation = rotation;
+    this.velocity = velocity;
+  }
+  
+  draw() {
+    this.sprite.draw();
+  }
+  
+  update(deltaTime) {
+    // Update the entity's position based on the velocity and time
+    const displacement = this.velocity.multiply(deltaTime);
+    this.sprite.position = this.sprite.position.add(displacement);
+  }
+}
