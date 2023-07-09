5fdf98fafc59f4e8407d5feaf1062db88394c135
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-07-09 21:42:48 +0200

    Update Alien.js
    GPT: Make appropriate changes to Alien class so that it's a subclass of GameEntity: [then I copied in the old class code]

--- JS game engine/Alien.js
+++ JS game engine/Alien.js
@@ -1,14 +1,10 @@
-class Alien {
+class Alien extends GameEntity {
   static imageUrl = 'alien-ship.png';
   static size = new Vector2D(50, 50);
   static health = 100;
 
   constructor(position) {
-    this.sprite = new Sprite(Alien.imageUrl, position, Alien.size);
+    super(position, 0, new Vector2D(0, 0), Alien.size, Alien.imageUrl);
     this.health = Alien.health;
-  }
-
-  draw() {
-    this.sprite.draw();
   }
 }
