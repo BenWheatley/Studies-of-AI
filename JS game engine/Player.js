3f16eaf78f1378d34c46572e89a02c2e676561f7
Ben Wheatley <BenWheatley@users.noreply.github.com>
2023-07-09 22:26:20 +0200

    Update Player.js
    Prompt to GPT: actually no; but never mind, replace decelerate with reverseThrust
    
    [Note: no idea where those new values in health, rotationalSpeed etc. come from; still, nice to see it refactoring itself]

--- JS game engine/Player.js
+++ JS game engine/Player.js
@@ -1,44 +1,42 @@
 class Player extends GameEntity {
   static imageUrl = 'player-ship.png';
   static size = new Vector2D(48, 48);
-  static acceleration = 0.1;
-  static deceleration = 0.05;
+  static health = 100;
+  static forwardAcceleration = 0.5;
+  static backwardAcceleration = 0.3;
   static maxSpeed = 5;
-  static rotationalSpeed = 0.1;
+  static rotationalSpeed = Math.PI / 180 * 3;
 
   constructor() {
     super(new Vector2D(0, 0), 0, new Vector2D(), Player.size, Player.imageUrl);
   }
 
-  accelerateForward(deltaTime) {
-    const accelerationVector = new Vector2D(
-      Math.cos(this.sprite.rotation) * Player.acceleration,
-      Math.sin(this.sprite.rotation) * Player.acceleration
-    );
+  accelerate(deltaTime) {
+    const accelerationVector = Vector2D.fromAngle(this.sprite.rotation).multiply(Player.forwardAcceleration);
     const velocityChange = accelerationVector.multiply(deltaTime);
     this.velocity = this.velocity.add(velocityChange);
-    if (this.velocity.mag() > Player.maxSpeed) {
+    this.clampSpeed();
+  }
+
+  reverseThrust(deltaTime) {
+    const decelerationVector = Vector2D.fromAngle(this.sprite.rotation + Math.PI).multiply(Player.backwardAcceleration);
+    const velocityChange = decelerationVector.multiply(deltaTime);
+    this.velocity = this.velocity.add(velocityChange);
+    this.clampSpeed();
+  }
+
+  turnLeft() {
+    this.sprite.rotation -= Player.rotationalSpeed;
+  }
+
+  turnRight() {
+    this.sprite.rotation += Player.rotationalSpeed;
+  }
+
+  clampSpeed() {
+    const speed = this.velocity.magnitude();
+    if (speed > Player.maxSpeed) {
       this.velocity = this.velocity.normalize().multiply(Player.maxSpeed);
     }
   }
-
-  decelerate(deltaTime) {
-    const decelerationVector = this.velocity.normalize().multiply(-Player.deceleration);
-    const velocityChange = decelerationVector.multiply(deltaTime);
-    if (velocityChange.mag() > this.velocity.mag()) {
-      this.velocity = new Vector2D();
-    } else {
-      this.velocity = this.velocity.add(velocityChange);
-    }
-  }
-
-  turnLeft(deltaTime) {
-    this.sprite.rotation -= Player.rotationalSpeed * deltaTime;
-  }
-
-  turnRight(deltaTime) {
-    this.sprite.rotation += Player.rotationalSpeed * deltaTime;
-  }
 }
-
-// Other Player class methods or properties
